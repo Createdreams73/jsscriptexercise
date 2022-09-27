5d69ad8751e3e7e861dcf1875d7a854794316e58
miranda mason <jmas1229@yahoo.com>
2022-09-27 10:52:50 -0400

    jsscriptexercis

---
+++ script.js
@@ -0,0 +1,86 @@
+// 1
+console.log(3);
+//2
+/*
+Number
+String
+Boolean 
+Undefined
+*/
+
+// 3
+/* 
+Addition is +
+Subtract is -
+Multiplication is *
+Division is /
+Modulus is %
+Exponentiation **
+
+Extras
+increment ++
+decrement--
+*/
+
+//4
+console.log(5 % 2);//1
+console.log(5 % 4);//1
+
+// Extra
+console.log( 4 % 5);//4
+
+//5
+console.log('I\'M MARY POPPINS, Y\ALL');
+
+//6
+console.log("HAWK" + "eye");
+
+//7
+console.log("Miranda".length + "Mason" .length);//12
+
+//8
+console.log("Avengers: Endgame"[16]);
+
+//BONUS
+//1
+console.log("\/");
+
+//2
+console.log(5 ** 4-9);
+//5*5*5*5=625
+//625-9=616
+
+//3
+console.log ("I".length + "AM".length + "Iron".length + "Man".length) % 10);
+//1 - 2 + 4 - 3=0
+//10 % 10 =0
+
+console.log ("I".length + "AM".length + "Iron".length + "Man".length) % 1);
+
+
+console.log ("I".length + "AM".length + "Iron".length + "Man".length) * 0);
+// 1 + 2 + 4 + 3 = 10
+//10 * 0 =0
+
+//4
+console.log("CAP"[1]);
+console.log("VISION"[0]);
+console.log("OKOEYE"[4]);
+console.log("NICK FURY"[0]);
+console.log("GROOT"[0]);
+console.log("DOCTOR STRANGE"[13]);
+console.log("THOR"[3]);
+console.log("STAR-LORD"[0]);
+
+//5
+var m = 25;
+var c = 4;
+var u = m + c;
+console.log(u);
+
+//WITH LET
+//let m = 25;
+//let c = 4;
+//let u = m + c;
+//comsole.log(u);
+
